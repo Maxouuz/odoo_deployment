9ac7232cf12145fbb44a71fcae9546200f255f6a
Benoit Dessaint <benoit.dessaint.etu@univ-lille.fr>
2023-03-13 16:32:03 +0100

    transfer_docker

---
+++ annexe_script_sae_S3/scripts/transfer_docker.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+VMIUT=/home/public/vm/bin/vmiut
+IP=$1
+NAME=$2
+COMPLETE_IP=192.168.194.$IP
+OUT=/tmp
+
+vm_start(){ 
+        if [ $($VMIUT info $NAME | grep -c running) -eq 0 ];
+                then
+                        $($VMIUT start $NAME > /dev/null 2>&1)
+                        printf "Démarrage de la machine\n" 
+                else printf "Déja démarrée\n" 
+        fi
+	
+        scp -r install_docker.sh user@$COMPLETE_IP:$OUT
+
+        return 0
+}
+
+launch_install(){
+	printf "\t\tConnexion à  root (mdp: root) \n"
+        ssh -t user@$COMPLETE_IP "su - root -c \" mv /tmp/install_docker.sh ~/ \" chmod u+x install_docker.sh \"sh install_docker.sh \"" 
+	ssh -t user@$COMPLETE_IP "su - root -c \" chmod u+x install_docker.sh \""
+	ssh -t user@$COMPLETE_IP "su - root -c \" sh install_docker.sh \""
+}
+
+
+vm_start
+launch_install
+exit 0
