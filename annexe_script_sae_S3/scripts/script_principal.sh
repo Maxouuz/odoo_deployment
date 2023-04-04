4d863324fec0506208ad75b2014b9e096afc8329
Maxence Stievenard <maxence.stievenard.etu@univ-lille.fr>
2023-04-04 14:48:03 +0200

    MAJ script principal car plus de machine traefik

--- annexe_script_sae_S3/scripts/script_principal.sh
+++ annexe_script_sae_S3/scripts/script_principal.sh
@@ -6,7 +6,6 @@ creation(){
    sh create_config.sh odoo 51 /home/public/vm/disque-bullseye-11.6-10go.vdi
    sh transfer_docker.sh 51 odoo
    sh create_config.sh dataBase 52
-   sh create_config.sh traefik 53
    sh create_config.sh saves 54
 
 }
